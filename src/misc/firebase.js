ce1793596f2110ac3393237b320650bb76521415
Deepti Singh <deepti.singh_cs19@gla.ac.in>
2023-12-28 11:42:39 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app';
+const config = {
+  apiKey: 'AIzaSyC7_khm41MXlxqYrY56NOTMdIZcdCSATis',
+  authDomain: 'chat-web-app-46de4.firebaseapp.com',
+  projectId: 'chat-web-app-46de4',
+  storageBucket: 'chat-web-app-46de4.appspot.com',
+  messagingSenderId: '43260014958',
+  appId: '1:43260014958:web:0b010c7d8b296bfb9b117f',
+};
+
+const app = firebase.initializeApp(config);
